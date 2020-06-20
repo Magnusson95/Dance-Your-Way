6238d38ffba75ecba654d008dbd851186a719150
Magnusson95 <alexander.magnusson95@gmail.com>
2020-06-20 15:52:47 +0000

    added googlemaps python import to requirements

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import os
+import googlemaps
+from datetime import datetime
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
@@ -9,6 +11,12 @@ app.config["MONGO_DBNAME"] = 'events_manager'
 app.config["MONGO_URI"] = 'mongodb+srv://prelaunch:prelaunch@danceyourway-fmkw8.mongodb.net/events_manager?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
+
+
+gmaps = googlemaps.Client(key='AIzaSyCWZTBCpz1s2iN98QZxLZd_pBmYWWu1kUs')
+
+# Geocoding an address
+geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
 
 
 @app.route('/')
@@ -31,6 +39,11 @@ def add_event():
     return render_template("add-event.html", events=mongo.db.events.find())
 
 
+@app.route('/test')
+def test():
+    return render_template("test.html")
+
+
 @app.route('/insert-event', methods=['POST'])
 def insert_event():
     events = mongo.db.events
@@ -42,3 +55,6 @@ if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
+
+
+# Geocode request: https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyCWZTBCpz1s2iN98QZxLZd_pBmYWWu1kUs
