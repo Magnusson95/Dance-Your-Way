9d1c652c76e6baf5ddd4519fbd73968de06ff4f3
Magnusson95 <alexander.magnusson95@gmail.com>
2020-06-22 10:30:18 +0000

    Created gitignore and added environment variables. Imported boto3 to link aws s3 for file upload and linked to form submission. Fixed card image formatting and added card reveal background image, all linked to s3 storage

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 import os
 import googlemaps
+import boto3
 from datetime import datetime
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from botocore.client import Config
+from os import path
+if path.exists("env.py"):
+    import env
+
 
 app = Flask(__name__)
 
@@ -49,18 +55,22 @@ def add_event():
     return render_template("add-event.html", events=mongo.db.events.find())
 
 
+ACCESS_KEY_ID = os.environ.get('ACCESS_KEY_ID')
+ACCESS_SECRET_KEY = os.environ.get('ACCESS_SECRET_KEY')
+
+
 @app.route('/insert-event', methods=['POST'])
 def insert_event():
     events = mongo.db.events
     events.insert_one(request.form.to_dict())
+    s3 = boto3.resource('s3', aws_access_key_id=ACCESS_KEY_ID, aws_secret_access_key=ACCESS_SECRET_KEY)
+    s3.Bucket('dance-your-way-event-images').put_object(Key=request.form['event_image'], Body=request.files['event_image_s3'])
     return redirect(url_for('event_added'))
-
 
 
 @app.route('/event-added')
 def event_added():
     return render_template("event-added.html", events=mongo.db.events.find())
-
 
 
 if __name__ == '__main__':
