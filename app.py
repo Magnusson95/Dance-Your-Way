bb436f1698b8a0dd30eac14019d58e05549bc0b4
Magnusson95 <alexander.magnusson95@gmail.com>
2020-06-23 07:53:44 +0000

    added basic signup function, linked to mongodb with method for checking if username exists

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
 import boto3
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, flash, session
 from flask_pymongo import PyMongo
+from wtforms import Form, BooleanField, TextField, PasswordField, validators
+from passlib.hash import sha256_crypt
 from bson.objectid import ObjectId
 from botocore.client import Config
 from os import path
@@ -17,13 +19,40 @@ ACCESS_SECRET_KEY = os.environ.get('ACCESS_SECRET_KEY')
 app.secret_key = os.environ.get('secret_key')
 app.config["MONGO_DBNAME"] = 'events_manager'
 app.config["MONGO_URI"] = 'mongodb+srv://prelaunch:prelaunch@danceyourway-fmkw8.mongodb.net/events_manager?retryWrites=true&w=majority'
-
 mongo = PyMongo(app)
+
+
+class RegistrationForm(Form):
+    username = TextField('Username', [validators.Length(min=4, max=20)])
+    email = TextField('Email Address', [validators.Length(min=6, max=50)])
+    password = PasswordField('New Password', [
+        validators.Required(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
+    accept_tos = BooleanField('I accept the Terms of Service and Privacy Notice (updated Jan 22, 2015)', [validators.Required()])
 
 
 @app.route('/')
 def home():
     return render_template("index.html", events=mongo.db.events.find())
+
+
+@app.route('/signup')
+def signup():
+    return render_template("signup.html")
+
+
+@app.route('/adduser', methods=['POST', 'GET'])
+def adduser():
+    organisers = mongo.db.organisers
+    find_organiser = organisers.find_one({'username': request.form['username']})
+
+    if find_organiser is None:
+        organisers.insert_one({'username': request.form['username'], 'password': request.form['password']})
+        return "<h1>You have Signed Up</h1>"
+    else:
+        return "<h1>This username already exists</h1>"
 
 
 @app.route('/get_salsa_events')
