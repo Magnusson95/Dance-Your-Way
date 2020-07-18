b245d1176b4dacaecb4ffd36251d6a439594f5e1
Magnusson95 <alexander.magnusson95@gmail.com>
2020-07-18 09:37:47 +0000

    added comments to app.py functions

--- app.py
+++ app.py
@@ -14,6 +14,9 @@ if path.exists("env.py"):
 
 app = Flask(__name__)
 
+'''
+Secret keys pulled from env.py
+'''
 
 ACCESS_KEY_ID = os.environ.get('ACCESS_KEY_ID')
 ACCESS_SECRET_KEY = os.environ.get('ACCESS_SECRET_KEY')
@@ -26,6 +29,11 @@ mongo = PyMongo(app)
 
 @app.route('/')
 def home():
+    '''
+    function to render the homepage, with events filtered to the current
+    weekday
+    '''
+
     weekdays = ("Monday", "Tuesday", "Wednesday", "Thursday",
                 "Friday", "Saturday", "Sunday")
     events = mongo.db.events.find(
@@ -45,6 +53,11 @@ def home():
 
 @app.route('/filtered_index', methods=['POST', 'GET'])
 def filtered_index():
+    '''
+    function to render the index page, but with a filter on events within a
+    specific country
+    '''
+
     if request.form["country"] == "Please select a country":
         weekdays = ("Monday", "Tuesday", "Wednesday", "Thursday",
                     "Friday", "Saturday", "Sunday")
@@ -94,6 +107,11 @@ def filtered_index():
 
 @app.route('/signup')
 def signup():
+    '''
+    function to render the sign up and log in forms, unless the user is
+    already in a session
+    '''
+
     if 'logged' in session:
         flash('You are already logged in as ' + session['username'])
         return redirect(url_for('home'))
@@ -102,11 +120,20 @@ def signup():
 
 @app.route('/terms&conditions')
 def terms():
+    '''
+    function to render terms and conditions
+    '''
+
     return render_template("terms.html")
 
 
 @app.route('/adduser', methods=['POST', 'GET'])
 def adduser():
+    '''
+    function to allow a user to sign up and add information into MongoDB and
+    start a session with these details
+    '''
+
     organisers = mongo.db.organisers
     find_organiser = organisers.find_one(
         {'username': request.form['username']})
@@ -135,6 +162,11 @@ def adduser():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    '''
+    function to allow a user to sign in to a session aligning to an account
+    already in MongoDB
+    '''
+
     if 'logged' in session:
         flash('Welcome back ' + session['username'])
         return redirect(url_for('add_event'))
@@ -161,6 +193,10 @@ def login():
 
 @app.route('/account')
 def account():
+    '''
+    function to allow a user to view their account information
+    '''
+
     if 'logged' in session:
         current_user = session['username']
         find_user = mongo.db.organisers.find_one({'username': current_user})
@@ -183,13 +219,16 @@ def sign_out():
     function to allow a user to sign out of the current session
     '''
 
-    session.clear()  # Clear session, notify user and redirect to index
+    session.clear()  # Clear session, notify user and redirect to home
     flash('You are now signed out')
     return redirect(url_for('home'))
 
 
 @app.route('/edituser/<organiser_username>', methods=['POST'])
 def edituser(organiser_username):
+    '''
+    function to allow a user to update their account information in MongoDB
+    '''
     organisers = mongo.db.organisers
     organisers.update({"username": organiser_username},
                       {
@@ -202,6 +241,7 @@ def edituser(organiser_username):
         'about': request.form.get('about'),
         'city': request.form.get('city'),
         'country': request.form.get('country'),
+        't&c': "on"
     }
     )
     if request.form.get('image-check') == "no change":
@@ -219,6 +259,10 @@ def edituser(organiser_username):
 
 @app.route('/delete_profile/<organiser_id>')
 def delete_profile(organiser_id):
+    '''
+    function to allow a user to remove their account from MongoDB, as long as
+    their session details equal the username in MongoDB
+    '''
     current_user = session['username']
     mongo.db.organisers.remove({'username': current_user})
     return redirect(url_for('sign_out'))
@@ -226,6 +270,9 @@ def delete_profile(organiser_id):
 
 @app.route('/add-event')
 def add_event():
+    '''
+    function to render a form for event information
+    '''
     if 'logged' in session:
         current_user = session['username']
         find_user = mongo.db.organisers.find_one({'username': current_user})
@@ -242,6 +289,9 @@ def add_event():
 
 @app.route('/insert-event', methods=['POST'])
 def insert_event():
+    '''
+    function to allow a user to upload event form information to MongoDB
+    '''
     events = mongo.db.events
     gmaps_key = googlemaps.Client(key=GOOGLE_ACCESS_KEY)
     geocode_result = gmaps_key.geocode(request.form.get('address')+request.form.get('city')+request.form.get('country'))
@@ -262,6 +312,9 @@ def insert_event():
 
 @app.route('/edit_event/<event_id>')
 def edit_event(event_id):
+    '''
+    function to render event form information, with data pulled from MongoDB
+    '''
     the_event = mongo.db.events.find_one({"_id": ObjectId(event_id)})
     current_user = session['username']
     find_user = mongo.db.organisers.find_one({'username': current_user})
@@ -276,6 +329,9 @@ def edit_event(event_id):
 
 @app.route('/update_event/<event_id>', methods=['POST'])
 def update_event(event_id):
+    '''
+    function to replace event information in MongoDB
+    '''
     events = mongo.db.events
     gmaps_key = googlemaps.Client(key=GOOGLE_ACCESS_KEY)
     geocode_result = gmaps_key.geocode(request.form.get('address')+request.form.get('city')+request.form.get('country'))
@@ -317,6 +373,10 @@ def update_event(event_id):
 
 @app.route('/delete_event/<event_id>')
 def delete_event(event_id):
+    '''
+    function to remove event from MongoDB, as long as the session username is
+    the same as the username in the event item
+    '''
     current_user = session['username']
     mongo.db.events.remove({
                             '_id': ObjectId(event_id),
@@ -328,11 +388,19 @@ def delete_event(event_id):
 
 @app.route('/style')
 def style():
+    '''
+    function to render page offering user option to select from 3 dance styles
+    '''
     return render_template("style.html")
 
 
 @app.route('/get_salsa_events')
 def get_salsa_events():
+    '''
+    function to pull all events from the database which contain Salsa, also
+    sorts countries to allow dropdown menu for users to select a specific
+    country
+    '''
     events = mongo.db.events.find()
     countries = mongo.db.countries.find().sort("country_name")
     return render_template(
@@ -344,6 +412,11 @@ def get_salsa_events():
 
 @app.route('/filtered_salsa', methods=['POST', 'GET'])
 def filtered_salsa():
+    '''
+    function to pull all events from the database which contain Salsa and a
+    specific country the user has selected, also sorts countries to allow
+    dropdown menu for users to select a specific country
+    '''
     if request.form["country"] == "Please select a country":
         events = mongo.db.events.find()
         countries = mongo.db.countries.find().sort("country_name")
@@ -370,6 +443,11 @@ def filtered_salsa():
 
 @app.route('/get_bachata_events')
 def get_bachata_events():
+    '''
+    function to pull all events from the database which contain Bachata, also
+    sorts countries to allow dropdown menu for users to select a specific
+    country
+    '''
     events = mongo.db.events.find()
     countries = mongo.db.countries.find().sort("country_name")
     return render_template(
@@ -381,6 +459,11 @@ def get_bachata_events():
 
 @app.route('/filtered_bachata', methods=['POST', 'GET'])
 def filtered_bachata():
+    '''
+    function to pull all events from the database which contain Bachata and a
+    specific country the user has selected, also sorts countries to allow
+    dropdown menu for users to select a specific country
+    '''
     if request.form["country"] == "Please select a country":
         events = mongo.db.events.find()
         countries = mongo.db.countries.find().sort("country_name")
@@ -407,6 +490,11 @@ def filtered_bachata():
 
 @app.route('/get_kizomba_events')
 def get_kizomba_events():
+    '''
+    function to pull all events from the database which contain Kizomba, also
+    sorts countries to allow dropdown menu for users to select a specific
+    country
+    '''
     events = mongo.db.events.find()
     countries = mongo.db.countries.find().sort("country_name")
     return render_template(
@@ -418,6 +506,11 @@ def get_kizomba_events():
 
 @app.route('/filtered_kizomba', methods=['POST', 'GET'])
 def filtered_kizomba():
+    '''
+    function to pull all events from the database which contain Kizomba and a
+    specific country the user has selected, also sorts countries to allow
+    dropdown menu for users to select a specific country
+    '''
     if request.form["country"] == "Please select a country":
         events = mongo.db.events.find()
         countries = mongo.db.countries.find().sort("country_name")
@@ -444,6 +537,10 @@ def filtered_kizomba():
 
 @app.route('/get_organisers')
 def get_organisers():
+    '''
+    function returning an accordion of all users in the database, and all
+    events linked to that organiser
+    '''
     organiser = mongo.db.organisers.find().sort("username")
     events = mongo.db.events.find()
     return render_template(
@@ -455,6 +552,9 @@ def get_organisers():
 
 @app.route('/organiser/<organiser_username>')
 def organiser(organiser_username):
+    '''
+    function to render a page with information on a selected organiser
+    '''
     _organiser = mongo.db.organisers.find_one({"username": organiser_username})
     _event = mongo.db.events.find({"username": organiser_username})
     return render_template(
